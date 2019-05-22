1fa0a498f7e5d9243af5cbd3b48aac1098b8daae
Oswalman <oswaldo-007@hotmail.com>
2019-05-22 12:31:19 -0500

    CRUD completo

--- api/controllers/ProductsController.js
+++ api/controllers/ProductsController.js
@@ -30,8 +30,28 @@ module.exports = {
         .then(products=>res.ok(products))
         .catch(err => res.notFound(err));
     },
-    delete(req,res){},
-    update(req,res){}
+    delete(req,res){
+        Products.destroy({
+            id:req.params.id
+        })
+        .then(products =>res.ok(products))
+        .catch(err => res.serverError(err));
+    },
+    update(req,res){
+        let attributes={};
+        if(req.param('ProductName'))
+        {
+            attributes.ProductName=req.param('ProductName')
+        }
+        Products.update({
+            id:req.params.id
+        },attributes
+        )
+        .then(products=>{
+            res.ok(products);
+        })
+        .catch(err=> res.serverError(err));
+    }
     
   
 
