d5baba6324816295473cd8f414d80ad14b66bddb
Oswalman <oswaldo-007@hotmail.com>
2019-05-22 11:03:51 -0500

    Sincronización de la base de datos con el api

--- api/controllers/ProductsController.js
+++ api/controllers/ProductsController.js
@@ -8,8 +8,27 @@
 module.exports = {
 
     create(req, res){
-        
-    }
+        Products.create({
+            ProductID: req.param('ProductID'),
+            ProductName:req.param('ProductName'),
+            SupplierID: req.param('SupplierID'),
+            CategoryID:req.param('CategoryID'),
+            QuantityPerUnit: req.param('QuantityPerUnit'),
+            UnitPrice:req.param('UnitPrice'),
+            UnitsInStock: req.param('UnitsInStock'),
+            UnitsOnOrder:req.param('UnitsOnOrder'),
+            ReorderLevel: req.param('ReorderLevel'),
+            Discontinued:req.param('Discontinued'),
+        })
+        .then(products=>{
+            return res.ok()
+        })
+        .catch(err => res.serverError(err));
+    },
+    find(req,res){},
+    delete(req,res){},
+    update(req,res){}
+    
   
 
 };
--- api/models/Products.js
+++ api/models/Products.js
@@ -6,8 +6,10 @@
  */
 
 module.exports = {
+  tableName:'products',
 
   attributes: {
+    
 
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
@@ -22,6 +24,49 @@ module.exports = {
     //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
     //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
     //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    ProductID: {
+      type: 'integer',
+      unique: true,
+      autoIncrement: true,
+      columnName: 'ProductID'
+    },
+    ProductName:{
+      type: 'string',
+      columnName: 'ProductName'
+    },
+    SupplierID:{
+      type: 'integer',
+      columnName: 'SupplierID'
+    },
+    CategoryID:{
+      type: 'integer',
+      columnName: 'CategoryID'
+    },
+    QuantityPerUnit:{
+      type: 'string',
+      columnName: ' QuantityPerUnit'
+    },
+    UnitPrice:{
+      type: 'integer',
+      columnName: 'UnitPrice'
+    },
+    UnitsInStock:{
+      type: 'integer',
+      columnName: 'UnitsInStock'
+    },
+    UnitsOnOrder:{
+      type: 'integer',
+      columnName: 'UnitsOnOrder'
+    },
+    ReorderLevel:{
+      type: 'integer',
+      columnName: 'ReorderLevel'
+    },
+    Discontinued:{
+      type: 'integer',
+      columnName: 'Discontinued'
+    },
+
 
   },
 
